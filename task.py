3454f061d30a5475fc22e8c61c68905a7faa7d53
pheav-dara <pheavdara0@gmail.com>
2025-09-18 23:00:00 +0000

    day 1

---
+++ task.py
@@ -0,0 +1,28 @@
+# Write your code below this line 👇
+print("Hello World!")
+
+# String "TEXT INSIDE DUBLE QUTE IS STRING"
+
+# 1.print("") Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.
+# 2. Knead the dough for 10 minutes.
+# 3. Add 3g of Salt.
+# 4. Leave to rise for 2 hours.
+# 5. Bake at 200 degrees C for 30 minutes.
+
+print("Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("Knead the dough for 10 minutes.")
+print("Add 3g of Salt.")
+print("Leave to rise for 2 hours.")
+print("Bake at 200 degrees C for 30 minutes.")
+
+# Fix the code below 👇
+
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \\ and the letter n ")
+
+# write code input() function
+name = input("What is your name? ")
+print("Hello " + name)
